0fd9dfbd6df2f120b0e08d4567ce6c5ccfc56967
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-07-04 20:45:01 -0300

    certo aqui

--- core/views.py
+++ core/views.py
@@ -1,7 +1,19 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
+from .models import Convidado
 
-class IndexView(TemplateView):
+
+
+class IndexView(CreateView):
     template_name = 'core/index.html'
+    model = Convidado
+    fields = ['nome_convidado', 'quantidade_convidados', ]
+
+
+class ConvidadoList(LoginRequiredMixin, ListView):
+    pass
+
 
 
 index = IndexView.as_view()
